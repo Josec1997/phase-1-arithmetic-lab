361eb08f217f8dee3a74d6993322d08a02313db4
Jose Campos <Jose_cmps14@yahoo.com>
2022-11-02 23:33:13 -0500

    commit tree cb3bb50ba759943afd70ee89040e2dbc24e3fe3a

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const multiply = 62
+let num1 = 31
+let num2 = 2
+num1 * num2 == multiply
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 52
+let num4 = 8
+let mod = num3 % num4
+
+
+let max = Math.max(4,8,12,16,20)
